5a0a026574681a988d36ba5036f9146fd53a7df7
tshubham-dev <168161863+tshubham-devlop@users.noreply.github.com>
2025-02-16 12:26:04 +0000

    dataTypes learn

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const id=12345;
+let personName ="shubham";
+var email="shubham@gmail.com";
+city='Mumbai';
+let state;
+
+//cannot change const
+/*avoid using var for scope related issue
+varibale can be declared wihtout any specification of datatype*/ 
+//no value assigned is undefine 
+
+console.table([id,personName,email,city,state])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+//code should be readble 
+/* cannot use alert() here because not in browser*/
+
+//data types
+ let name="shubham";  //string datatype ""
+ let age=21;   // number 2 to power something
+ let isLoggedIn=true; // boolean value t/F
+ //null => standalone value,object; null is a value to say nothnig like 0 in maths
+ //undefined=> no value assigned ;
+ //symbol => unique
+
+
+ //object
+ let a;
+ console.log( typeof age);
+ console.log(typeof null);
+ console.log( typeof a);
+ 
