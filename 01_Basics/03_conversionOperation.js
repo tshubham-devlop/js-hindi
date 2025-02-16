efb48102957dff520f5dbe4da0ab07f0033380b6
tshubham-dev <168161863+tshubham-devlop@users.noreply.github.com>
2025-02-16 13:07:43 +0000

    datatypes conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+//one type of data can be converted to another if required using data conversion
+// but always be carefull with values after conversion
+//eg;
+
+let naam=  false;
+let numberForm=Number(naam);
+// console.log(typeof naam);
+// console.log(typeof numberForm);
+// console.log(numberForm);
+
+let stringForm=String(naam);
+console.log(typeof naam);
+console.log(typeof stringForm);
+console.log(stringForm);
+
+// let boolForm=Boolean(naam);
+// console.log(typeof boolForm);
+// console.log(boolForm);
+
+
+
+/* using Number
+"33"=>33
+"shubham"=>NaN
+"true"=>1
+""=>  for boolean conv false
+" zuierbk756347i"=>true for boolean conv
+etc etc
+
+
+
+
+
+*/
+
+
